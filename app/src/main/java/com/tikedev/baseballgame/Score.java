1194e77cfedbe4c4acca56821701c1277ac438ee
tikepjt <tike@tikedev.com>
2018-07-30 22:49:51 +0900

    first commit

---
+++ app/src/main/java/com/tikedev/baseballgame/Score.java
@@ -0,0 +1,13 @@
+package com.tikedev.baseballgame;
+
+public class Score {
+    String title;
+    }
+    String getTitle(){
+        return this.title;
+    }
+    Score(String title){
+        this.title=title;
+    }
+}
+
